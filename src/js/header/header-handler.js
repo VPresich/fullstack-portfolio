285cbd0067f343e15f83c3333e683c19a5767939
VPresich <valya.alexandrova.presich@gmail.com>
2024-08-19 15:22:22 +0200

    fixed mobile-menu

--- src/js/header/header-handler.js
+++ src/js/header/header-handler.js
@@ -2,10 +2,13 @@ import { scrollToElement } from './scroll-to-element';
 
 const menuBtn = document.querySelector('.js-header-menu-btn');
 const headerMenuRef = document.querySelector('.js-header-menu');
+const headerOrderLinkRef = document.querySelector('.js-header-order-link');
 
 menuBtn && menuBtn.addEventListener('click', handleMenuOpen);
-
 headerMenuRef && headerMenuRef.addEventListener('click', handleScrollDocument);
+
+headerOrderLinkRef &&
+  headerOrderLinkRef.addEventListener('click', handleScrollDocument);
 
 function handleMenuOpen() {
   headerMenuRef && headerMenuRef.classList.toggle('isopen');
--- src/js/header/mobile-menu-handler.js
+++ src/js/header/mobile-menu-handler.js
@@ -4,13 +4,15 @@ const mobileMenuBtn = document.querySelector('.js-header-mobile-menu-btn');
 const mobileMenuRef = document.querySelector('.js-mobile-menu');
 const mobileMenuCloseBtn = document.querySelector('.js-mobile-menu-close-btn');
 const mobileNavRef = document.querySelector('.js-mobile-menu-nav');
+const mobileOrderLink = document.querySelector('.js-mobile-menu-order');
 
 mobileMenuBtn && mobileMenuBtn.addEventListener('click', handleOpenMobileMenu);
 
-// mobileMenuCloseBtn &&
-mobileMenuCloseBtn.addEventListener('click', handleCloseMobileMenu);
-
+mobileMenuCloseBtn &&
+  mobileMenuCloseBtn.addEventListener('click', handleCloseMobileMenu);
 mobileNavRef && mobileNavRef.addEventListener('click', handleScrollDocument);
+mobileOrderLink &&
+  mobileOrderLink.addEventListener('click', handleScrollDocument);
 
 function handleOpenMobileMenu() {
   mobileMenuRef.classList.add('is-open');
